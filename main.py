7cccb7e97dfcfe7ceb50177432d458ab79b2d357
Andy Crowe <crowemi@hotmail.com>
2021-09-06 11:41:32 -0700

    added email

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-import json
 import requests
-import os 
+import os
 import math
 from datetime import datetime, timedelta
 
-# from pco.people.person import Person
+import boto3
 
-APP_ID = os.getenv("PCO_APP_ID") 
-SECRET = os.getenv("PCO_SECRET") 
+
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+
+APP_ID = os.getenv("PCO_APP_ID")
+SECRET = os.getenv("PCO_SECRET")
 
 MEMBER_MANIFEST = list()
 
+client = boto3.client('ses', 'us-west-2', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+
 def main():
 
+    formatted_date = datetime.now().strftime("%A, %B %d, %Y")
+
+    get_member_list()
+
+    number_of_members = math.ceil(len(MEMBER_MANIFEST)/days_in_month())
+    today = datetime.now().day
+    email = str.strip(build_email(
+        MEMBER_MANIFEST[(datetime.now().day - 1):((today + number_of_members) - 1)],
+        formatted_date
+    ))
+
+    aws_email(
+        subject=f"Elder Daily Prayer - {formatted_date}",
+        content=email,
+        to_address=['crowemi@hotmail.com'],
+    )
+
+
+def get_member_list():
     member_iterator = 1
     offset = 0
-    process = True 
+    process = True
 
-    while process: 
+    while process:
         # authenticate
         resp = requests.get(
             f'https://api.planningcenteronline.com/people/v2/people?order=last_name&where[membership]=Member&where[status]=active&offset={offset}',
@@ -25,46 +50,76 @@ def main():
         )
 
         data = resp.json()["data"]
-        print(f"Active member count: {len(data)}")
 
-        if len(data) == 0: 
+        if len(data) == 0:
             # no more members exist, break out
             process = False
             continue
 
         for j in data:
             member = {
-                'id' : member_iterator,
+                'id': member_iterator,
                 'first_name': j["attributes"]["first_name"],
                 'last_name': j["attributes"]["last_name"],
                 'avatar': j["attributes"]["avatar"]
             }
-            print(member)
             MEMBER_MANIFEST.append(member)
             member_iterator += 1
 
-
         offset += 25
 
-    print(len(MEMBER_MANIFEST))
-    print(math.ceil(len(MEMBER_MANIFEST)/days_in_month()))
+
+def build_email(collection: list, formatted_date: str) -> str:
+    email = "<html>"
+    email += f'<h5>{formatted_date}</h5>'
+
+    notes = ""
+
+    for record in collection:
+        name = f"{record['first_name']} {record['last_name']}"
+        avatar = record['avatar']
+
+        email += f'<table><tr><td style="vertical-align: text-top; padding:2px;"><img height="100" width="100" src="{avatar}"></td><td><table><tr><td style="vertical-align: text-top; padding:2px;"><h5>{name}</h5></td></tr><tr>{notes}</tr></table></td></tr></table>'
+
+    email += "</html>"
+    return email
 
 
-def days_in_month(): 
+def days_in_month():
     today = datetime.now()
     current_day = datetime(year=today.year, month=today.month, day=1)
-    end_of_month = False 
+    end_of_month = False
     days_in_month = 1
-    
-    while end_of_month == False:
+
+    while not end_of_month:
         next_day = current_day + timedelta(days=1)
         if next_day.month != current_day.month:
             end_of_month = True
-            continue 
+            continue
         days_in_month += 1
         current_day = next_day
 
     return days_in_month
+
+
+def aws_email(subject: str, to_address: list, content: str):
+    resp = client.send_email(
+        Source='no-reply@crowemi.com',
+        Destination={
+            'ToAddresses': to_address
+        },
+        Message={
+            'Subject': {
+                'Data': subject
+            },
+            'Body': {
+                'Html': {
+                    'Data': content
+                }
+            }
+        }
+    )
+
 
 if __name__ == "__main__":
     main()
